b993bc2f0f83e9b2579dd25e63143908f2d0dce5
Olena <newsroommy@gmail.com>
2023-06-26 18:49:05 +0300

    add in main

--- src/modules/Main/Main.jsx
+++ src/modules/Main/Main.jsx
@@ -3,15 +3,14 @@ const Main = () => {
   return (
     <main>
       <section className={css.sectionMain}>
-        <div>
-          <p className={css.text}>A HAIR SALON FOR MEN IN KYIV</p>
-        </div>
+        <p className={css.text}>A HAIR SALON FOR MEN IN KYIV</p>
         <div className={css.mainWrapper}>
           <h1 className={css.title}>BarberShop</h1>
-          <p className={css.text}>
-            Ми експерти в модних зачісках для чоловіків. Працюємо швидко,
-            обережно і зі смаком
-          </p>
+          <div className={css.textWrapper}>
+            <p className={css.text}>
+              Ми експерти в модних зачісках для чоловіків.
+            </p>
+          </div>
         </div>
       </section>
     </main>
