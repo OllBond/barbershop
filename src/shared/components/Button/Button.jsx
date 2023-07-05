d629a75678e635b55bab83d644538b7b11196c18
Olena <newsroommy@gmail.com>
2023-07-05 11:38:28 +0300

    add validations form

--- src/shared/components/Button/Button.jsx
+++ src/shared/components/Button/Button.jsx
@@ -3,7 +3,7 @@ import css from "./button.module.css";
 const Button = ({ text, openModal }) => {
   return (
     <div>
-      <button className={css.btn} onClick={openModal}>
+      <button className={css.btn} onClick={openModal} type="submit">
         {text}
       </button>
     </div>
